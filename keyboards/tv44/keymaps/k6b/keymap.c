7ec152730c44a6f31137b332b173b7c996a0bb7e
Kyle Berry <kyle@kyleberry.com>
2016-08-23 12:40:56 -0700

    Adding Tap Layer to ESC
    Adding F keys to a tap layer that's accessed by holding down ESC. Tapping ESC
    the key will function as a normal ESC key.

--- keyboards/tv44/keymaps/k6b/keymap.c
+++ keyboards/tv44/keymaps/k6b/keymap.c
@@ -15,6 +15,7 @@ extern keymap_config_t keymap_config;
 #define _L1 3
 #define _L2 4
 #define _L3 5
+#define _L4 6
 
 // Macro name shortcuts
 #define QWERTY M(_QW)
@@ -39,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * `------------------------------------------------------------------------------'
   */
   [_QW] = {
-    {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC },
+    {LT(_L4), KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC },
     {MO(_L1), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, MO(_L1) },
     {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_F,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MO(_L2) },
     {KC_LCTL, MO(_L2), KC_LGUI, KC_ENT,  XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,  KC_RALT, KC_RSFT, XXXXXXX, TG(_L3) }
@@ -112,27 +113,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {_______, KC_TAB,  KC_ESC,  _______, _______, _______, _______,  BL_DEC, KC_PGDN,  KC_END, KC_VOLD, KC_MUTE },
     {_______, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, _______, BL_TOGG, _______, XXXXXXX, _______ }
   },
+  /* Tap Function - This layer contains F keys on a tap layer accessed by holding ESC
+  * ,------------------------------------------------------------------------------.
+  * |     | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 |            |
+  * |------------------------------------------------------------------------------|
+  * |         | F11 | F12 | F13 | F14 | F15 | F16 | F17 | F18 | F19 | F20 |        |
+  * |------------------------------------------------------------------------------|
+  * |            |     |     |     |     |     |     |     |     |     |     |     |
+  * |------------------------------------------------------------------------------|
+  * |         |        |      |              |          |      |        |          |
+  * `------------------------------------------------------------------------------'
+  */
+  [_L4] = { /* LAYER 4 - Tap Function Layer */
+    {_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______ },
+    {_______, KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  _______ },
+    {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
+    {_______, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, XXXXXXX, _______ }
+  },
   /* LOCK
   * ,------------------------------------------------------------------------------.
-  * |     | F1  | F2  | F3  | F4  |     |  +  |  7  |  8  |  9  |  0  |            |
+  * |     |     |     |     |     |     |  +  |  7  |  8  |  9  |  0  |            |
   * |------------------------------------------------------------------------------|
-  * |         | F5  | F6  | F7  | F8  |     |  -  |  4  |  5  |  6  |  =  |        |
+  * |         |     |     |     |     |     |  -  |  4  |  5  |  6  |  =  |        |
   * |------------------------------------------------------------------------------|
-  * |            | F9  | F10 | F11 | F12 |  /  |  *  |  1  |  2  |  3  |Enter|     |
+  * |            |     |    |    |    |  /  |  *  |  1  |  2  |  3  |Enter|     |
   * |------------------------------------------------------------------------------|
   * |         |        |      |              |          |      |        |          |
   * `------------------------------------------------------------------------------'
   */
   [_L3] = { /* LAYER 3 - LOCK */
-    {_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, KC_PPLS, KC_P7,   KC_P8,   KC_P9,   KC_P0,   _______ },
-    {_______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, KC_PMNS, KC_P4,   KC_P5,   KC_P6,   KC_PEQL, _______ },
-    {_______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSLS, KC_PAST, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______ },
+    {_______, _______, _______, _______, _______, _______, KC_PPLS, KC_P7,   KC_P8,   KC_P9,   KC_P0,   _______ },
+    {_______, _______, _______, _______, _______, _______, KC_PMNS, KC_P4,   KC_P5,   KC_P6,   KC_PEQL, _______ },
+    {_______, _______, _______, _______, _______, KC_PSLS, KC_PAST, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______ },
     {_______, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, XXXXXXX, _______ }
   }
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-
+  [6] = ACTION_LAYER_TAP_KEY(6, KC_ESC)
 };
 
 void persistant_default_layer_set(uint16_t default_layer) {
